59a701d417f42f3cac0e1d8e1c30c76e8d1c0377
weffology <stephlarkinrose@gmail.com>
2020-02-15 16:39:49 -0500

    added arrows to scroll up and down, more color

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,34 @@ $(document).ready(function () {
 
   $(window).scroll(scrollFunction);
   function scrollFunction() {
-    if ($(document).scrollTop() > 400) {
+    if ($(document).scrollTop() > 700) {
       $("nav").addClass("navScroll");
       $("nav a").addClass("navScroll");
       $("nav").removeClass("navbar-light");
       $("nav").removeClass("navStart");
       $("nav").addClass("navbar-dark");
+      // $("#buttonTop").removeClass("hideButton");
       $("#mainLogo").attr("src", "assets/img/weffology-logo-white.png");
-    } else if ($(document).scrollTop() < 400) {
+    } else if ($(document).scrollTop() < 700) {
       $("nav").removeClass("navScroll");
       $("nav a").removeClass("navScroll");
       $("nav").removeClass("navbar-dark");
       $("nav").addClass("navbar-light");
       $("nav").addClass("navStart");
+      // $("#buttonTop").addClass("hideButton");
       $("#mainLogo").attr("src", "assets/img/weffology-logo.png");
     }
   };
+
+  $(window).scroll(buttonAppear);
+  function buttonAppear() {
+    if ($(document).scrollTop() > 100) {
+      $("#buttonTop").removeClass("hideButton");
+    } else if ($(document).scrollTop() < 100) {
+      $("#buttonTop").addClass("hideButton");
+    }
+  };
+
 
   $("#web-dev-icon").hover(hoverInWeb, hoverOutWeb);
   function hoverInWeb() {
@@ -42,5 +54,9 @@ $(document).ready(function () {
   function hoverOutCamera() {
     $(this).attr("src", "assets/img/camera-icon.png");
   };
+
+  $("#buttonTop").click(function() {
+    window.scrollTo(0,0);
+  });
 
 });
